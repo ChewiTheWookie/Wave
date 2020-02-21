d8678852837f6d71999dec9351c18082b0012fca
Colin L <darkblade.ca@gmail.com>
2020-02-20 20:18:19 -0600

    Edited the way the window is created to be sized properly
    Signed-off-by: Colin L <darkblade.ca@gmail.com>

--- src/com/chewithewookie/wave/main/Window.java
+++ src/com/chewithewookie/wave/main/Window.java
@@ -1,32 +1,24 @@
 package com.chewithewookie.wave.main;
 
 import javax.swing.JFrame;
-import java.awt.Canvas;
-import java.awt.Dimension;
+import java.awt.*;
 
 public class Window extends Canvas {
 
-    public Window(int width, int height, String title, Game game){
-        int widthOffset, heightOffset;
+    public Window(Game game){
+        int width = Game.WIDTH;
+        int height = Game.HEIGHT;
 
-        if(System.getProperty("java.version").startsWith("1.8.")){
-            widthOffset = 6;
-            heightOffset = 29;
-        }else if(System.getProperty("java.version").startsWith("1.8.")){
-            widthOffset = 16;
-            heightOffset = 39;
-        } else{
-            widthOffset = 40;
-            heightOffset = 20;
-        }
+        JFrame frame = new JFrame(Game.TITLE);
 
+        frame.pack();
+        Insets inset = frame.getInsets();
+        width += inset.left + inset.right;
+        height += inset.top + inset.bottom;
 
-
-        JFrame frame = new JFrame(title);
-
-        frame.setPreferredSize(new Dimension(width + widthOffset, height + heightOffset));
-        frame.setMinimumSize(new Dimension(width + widthOffset, height + heightOffset));
-        frame.setMaximumSize(new Dimension(width + widthOffset, height + heightOffset));
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
